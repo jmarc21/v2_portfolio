bb49e0f22da7a538751cefcd7aae55ba4f6fd31c
jmarc21 <justindemarco21@gmail.com>
2020-08-31 16:16:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">Under Construction</div>
+      <div className="subtitle">Sorry for the inconvenience, but i am making some important changes the my portfolio. It will be up shortly.</div>
+      <div className="socialmedia">
+        <div className="instagram"></div>
+        <div className="github"></div>
+      </div>
     </div>
   );
 }
