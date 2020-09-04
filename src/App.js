951f0f2c55e9b3d2a4191b743d68b91266ec5a1d
jmarc21 <justindemarco21@gmail.com>
2020-09-04 12:19:41 -0600

    under construction page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="title">Under Construction</div>
-      <div className="subtitle">Sorry for the inconvenience, but i am making some important changes the my portfolio. It will be up shortly.</div>
-      <div className="socialmedia">
-        <div className="instagram"></div>
-        <div className="github"></div>
+      <div className="blur">
+        <div className="title">Under <br/> Construction</div>
+        <div className="subtitle">Sorry for the inconvenience, but I am making some important changes to my portfolio. It will be up shortly.</div>
+        <div className="percentage">
+          <p>Site Progress</p>
+          <div className="percentage-bar">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="social-media">
+          <a href="https://www.instagram.com/justindemarco21/" target="_blank"><div className="instagram"></div></a>
+          <a href="https://github.com/jmarc21" target="_blank"><div className="github"></div></a>
+        </div>
       </div>
     </div>
   );
